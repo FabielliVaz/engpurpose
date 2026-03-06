04fba9e21341711261bccc59a8e34bdcd142d4ed
Fabielli Vaz <fabiellivaz@gmail.com>
2026-03-06 19:24:55 -0300

    Remove comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,41 +8,22 @@ import Songs from './pages/Songs'
 import SongDetail from './pages/SongDetail'
 import QuickAccess from './pages/QuickAccess'
 
-function PrivateRoute({ children }: { children: React.ReactNode }) {
-  const { isAuthenticated, isLoading } = useAuth()
-
-  if (isLoading) {
-    return <div className="flex items-center justify-center min-h-screen bg-slate-50 text-gray-600">Carregando...</div>
-  }
-
-  return isAuthenticated ? <>{children}</> : <Navigate to="/login" />
-}
-
 function AppRoutes() {
   return (
     <>
       <SessionWarning />
       <SessionDebug />
       <Routes>
+
         <Route path="/" element={<QuickAccess />} />
+        
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/songs"
-          element={
-            //<PrivateRoute>
-              <Songs />
-            //</PrivateRoute>
-          }
-        />
-        <Route
-          path="/songs/:id"
-          element={
-            //<PrivateRoute>
-              <SongDetail />
-            //</PrivateRoute>
-          }
-        />
+        
+        <Route path="/songs" element={<Songs />} />
+        <Route path="/songs/:id" element={<SongDetail />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   )
